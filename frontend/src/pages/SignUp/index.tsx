2735d6be0cb3008802cfe70f38177f6be66b7830
Anderson Pacheco <andinpacheco16@gmail.com>
2020-07-02 12:50:07 -0300

    estrutura de páginas e usabilidade do input

---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import LogoImg from '../../assets/logo.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={LogoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="passowrd"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
